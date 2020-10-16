fac1e54e4b7a85917f05410ac3ef934890ff7b02
e <17638344413@163.com>
2020-10-16 21:57:16 +0800

    提交文件

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -25,7 +25,6 @@
   import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
   import DetailParamInfo from "./childComps/DetailParamInfo";
 
-
   import Scroll from "components/common/scroll/Scroll";
 
   import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
